0d426f40ff90fb7ab0fee8358e201a90358f1029
Tharun0102 <rallapallitharun0102@gmail.com>
2021-03-21 12:45:08 +0530

    added Minimizing Coins

---
+++ DynamicProgramming/MinimizingCoins.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class MinimizingCoins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // input
+        int n = sc.nextInt(), x = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        Arrays.sort(coins);
+        int[] dp = new int[x + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= x; i++) {
+            int minimum = Integer.MAX_VALUE;
+            for (int j = 0; j < n; j++) {
+                if (coins[j] > i)
+                    break;
+                minimum = Math.min(minimum, dp[i - coins[j]]);
+            }
+            dp[i] = (minimum == Integer.MAX_VALUE) ? minimum : 1 + minimum;
+        }
+        System.out.println((dp[x] == Integer.MAX_VALUE) ? -1 : dp[x]);
+        sc.close();
+    }
+}
