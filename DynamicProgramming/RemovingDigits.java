2c9f9e0be19a33190b7b9dba7b912821939bdcd7
Tharun0102 <rallapallitharun0102@gmail.com>
2021-04-17 15:12:54 +0530

    Removing digits

---
+++ DynamicProgramming/RemovingDigits.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class RemovingDigits {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // input
+    int n = sc.nextInt();
+
+    int result = 1;
+    while (n >= 10) {
+      int largestDigit = 0;
+      // find largest digit
+      int temp = n;
+      while (temp > 0) {
+        int currDigit = temp % 10;
+        largestDigit = Math.max(largestDigit, currDigit);
+        temp /= 10;
+      }
+      n -= largestDigit;
+      result++;
+    }
+    System.out.println(result);
+
+    sc.close();
+  }
+}
